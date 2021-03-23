ad25e36610676b480f31fe3bb3fc957014923a01
karim lemmouchi <karim.planb@gmail.com>
2021-03-23 17:04:24 +0100

    fill content

---
+++ src/containers/video-list.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import VideoListItem from '../components/video-list-item';
+
+const VideoList = ({movieList}) => {
+    console.log(movieList);
+    return  (
+        <div>
+            <ul>
+                {
+                    movieList.map(movie => {
+                        return <VideoListItem key={movie.id} movie={movie}></VideoListItem>
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default VideoList;
