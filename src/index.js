796ab52a0b737cbc1793481f3866d5a691ef27be
karim lemmouchi <karim.planb@gmail.com>
2021-03-23 14:32:59 +0100

    update index.js to point to App component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from './containers/app';
 
-const App = function () {
-    return <div>Salut les amis</div>
-}
+const API_key = "5d3ea7738a292dc76fd52915c6b54cdd";
 
 ReactDOM.render(<App></App>, document.querySelector('.container'));
